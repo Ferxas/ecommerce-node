8c60f1af5ad0f1ecfd76ad75d6e9a740db961817
Ferxas <ferxas321@hotmail.es>
2023-07-13 15:53:49 -0500

    adding interface-client

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -4,8 +4,11 @@ import userModel from '../models/userModel';
 export const requireSignIn = async (req, res, next) => {
 
     try {
-    const decode = Jwt.verify(req.headers.authorization, process.env.JwT_SECRET)
+    const decode = Jwt.verify(req.headers.authorization, process.env.JWT_SECRET);
+    req.user = decode;
     next();
+    
+
     } catch(error) {
         console.log(error)
     }
@@ -24,5 +27,10 @@ export const isAdmin = async (req, res, next) => {
         }
     } catch (error) {
         console.log(error)
+        res.status(401).send({
+            success: false,
+            error,
+            message: 'Error en admin middleware',
+        })
     }
 }
